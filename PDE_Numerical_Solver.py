e7aa212036ad1c658cd1a8bb7e242aa9c0f70252
jjwb22101 <rocketman1701e@gmail.com>
2020-02-27 15:34:39 -0500

    First Commit

---
+++ PDE_Numerical_Solver.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+dt = 0.001 #Timestep (seconds)
+
+thrust_file = open("Thrust.txt")
+drag_file = open("Drag.txt")
+gravity_file = open("Gravity.txt")
+mass_file = open("Mass.txt")
+thrust_torque_file = open("Torque_Thrust.txt")
+drag_torque_file = open("Torque_Drag.txt")
+gravity_torque_file = open("Torque_Gravity.txt")
+moment_of_intertia_file = open("MOI.txt")
+
